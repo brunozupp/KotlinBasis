0902b6012088751e039869bbbe5db7bc20c74be5
Bruno Noveli <brunimzuppwolf@gmail.com>
2023-12-06 16:01:36 -0300

    OOP - Casting and Checking Types

--- src/main/kotlin/oop/interfaces/ExecutionInterfaces.kt
+++ src/main/kotlin/oop/interfaces/ExecutionInterfaces.kt
@@ -31,4 +31,22 @@ fun main() {
     println("Número da sessão: ${providerTwo.getSessionId()}")
     print("Informações da pessoa: ")
     providerTwo.printInfo(person)
+
+    println("-------------------------------------------")
+
+    checkTypes(provider)
+    checkTypes(providerTwo)
+}
+
+fun checkTypes(infoProvider: IPersonInfoProvider) {
+    if(infoProvider is ISessionInfoProvider) {
+        println("É um ISessionInfoProvider")
+
+        val idOne = (infoProvider as ISessionInfoProvider).getSessionId()
+        println("Fazendo o casting para ISessionInfoProvider e pegando o id = $idOne")
+
+        println("Usando o smartcasting = ${infoProvider.getSessionId()}")
+    } else {
+        println("NÃO É um ISessionInfoProvider")
+    }
 }
