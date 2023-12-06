952267decd17dcfcf30850e16d6b35d4c349550c
Bruno Noveli <brunimzuppwolf@gmail.com>
2023-12-06 16:50:50 -0300

    OOP - Object Expression

--- src/main/kotlin/oop/inheritance/ExecutionInheritance.kt
+++ src/main/kotlin/oop/inheritance/ExecutionInheritance.kt
@@ -12,5 +12,26 @@ fun main() {
 
     // Because BasicInfoProvider has 'protected' in the property sessionBasicProvider I can't access it in an instance
     // println(provider.sessionBasicProvider)
+
+    println("\n----------------------------------------\n")
+
+    // OBJECT EXPRESSION
+    val providerObjectExpression = object : IPersonInfoProvider {
+        override val label: String
+            get() = "Provider defined as Object Expression"
+
+        override fun printInfo(person: Person) {
+            println("Apenas o nome = ${person.firstname}")
+        }
+
+        val newProperty : String = "With Object Expression I can define new properties"
+
+        fun newMethod() = println("With Object Expression I can define new methods")
+    }
+
+    println(providerObjectExpression.label)
+    providerObjectExpression.printInfo(person)
+    providerObjectExpression.newMethod()
+    println(providerObjectExpression.newProperty)
 }
 
