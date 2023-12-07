7660838fbaecd73d838333d6301d85881ed8779a
Bruno Noveli <brunimzuppwolf@gmail.com>
2023-12-07 14:45:30 -0300

    OOP - Data class

--- src/main/kotlin/oop/sealedClasses/EntityFactory.kt
+++ src/main/kotlin/oop/sealedClasses/EntityFactory.kt
@@ -40,6 +40,10 @@ sealed class Entity() {
         val name = "Help"
     }
 
+    // Data class in Kotlin is a concise and mutable data type that provides methods such as equals, hashCode and toString automatically
+    // So, it compares objects taking the value and not the address in the memory
+    // Another characteristic of data class is that it gives me a method to copy the instance (its values) and returning another instance (method - copy)
+    // I can override the methods that were generated automatically
     data class Easy(val id: String, val name: String) : Entity()
     data class Medium(val id: String, val name: String) : Entity()
     data class Hard(val id: String, val name: String, val multiplier: Float) : Entity()
@@ -66,4 +70,22 @@ fun main() {
     }
 
     println(msg)
+
+    // Example of comparison in a data class
+
+    val easy1 = Entity.Easy("id", "name")
+    val easy2 = Entity.Easy("id", "name")
+
+    println(if (easy1 == easy2) "instâncias são iguais em valor" else "instâncias são diferentes em valor")
+
+    // In a data class, if I need to compare the location in memory between the instances, I need to use ===
+    println(if (easy1 === easy2) "instâncias são iguais em memória" else "instâncias são diferentes em memória")
+
+    val easyCopy = easy1.copy();
+    println("easyCopy = id = ${easyCopy.id} | name = ${easyCopy.name}")
+
+    val easyCopyChangeIdValue = easy1.copy(
+        id = "ID MUDADO"
+    )
+    println("easyCopyChangeIdValue = id = ${easyCopyChangeIdValue.id} | name = ${easyCopyChangeIdValue.name}")
 }
